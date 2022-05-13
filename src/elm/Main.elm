ccc94983170cbdb133ee511ee526173e04dab2fb
gaby1994 <gabriel.degez@free.fr>
2022-05-13 10:05:20 +0200

    WIP

---
+++ src/elm/Main.elm
@@ -0,0 +1,199 @@
+module Main exposing (..)
+
+import Browser
+import Browser.Events
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import Html.Events as Events
+import Time exposing (Posix)
+import Setters
+import Update
+import Json.Decode as Decode
+import List exposing (indexedMap)
+
+{-| Got from JS side, and Model to modify -}
+type alias Flags = { now : Int }
+type alias Model =
+  { gameStarted : Bool
+  , lastUpdate : Int
+  , time : Int
+  , coloredSquare : Int
+  , snake : {
+        cases : List (Int, Int)
+      , direction : Int
+    }
+  }
+
+init : Flags -> ( Model, Cmd Msg )
+init { now } =
+  now
+  |> \time -> Model False time time 0 {cases = [(0, 0)], direction = 0}
+  |> Update.none
+
+{-| All your messages should go there -}
+type Key = ArrowUp | ArrowRight | ArrowDown | ArrowLeft | Space
+type Msg
+  = NextFrame Posix
+  | ToggleGameLoop
+  | KeyDown Key
+
+{-| Manage all your updates here, from the main update function to each
+ -|   subfunction. You can use the helpers in Update.elm to help construct 
+ -|   Cmds. -}
+updateSnake : Model -> Model
+updateSnake model =
+  let snake = model.snake
+      newSnake = {
+          snake | cases = List.map(\(x,y) -> 
+            if snake.direction == 0 then 
+              (x,modBy 40 (y - 1))
+            else
+            if snake.direction == 1 then
+              (modBy 40 (x+1),y)
+            else 
+            if snake.direction == 2 then
+              (x,modBy 40 (y + 1))
+            else
+              (modBy 40 (x - 1),y)
+
+          ) snake.cases
+
+        }
+  in
+    {
+       model | snake = newSnake
+    }
+
+toggleGameLoop : Model -> ( Model, Cmd Msg )
+toggleGameLoop ({ gameStarted } as model) =
+  not gameStarted
+  |> Setters.setGameStartedIn model
+  |> Update.none
+
+keyDown : Key -> Model -> ( Model, Cmd Msg )
+keyDown key model =
+  let snake = model.snake
+      newSnake = 
+        case key of 
+          ArrowUp -> 
+            { snake | direction = 0 }
+          ArrowDown -> 
+            { snake | direction = 2 }
+          ArrowLeft -> 
+            { snake | direction = 3 }
+          ArrowRight -> 
+            { snake | direction = 1 }
+          Space -> 
+            snake
+  in
+    ({ model | snake = newSnake }, Cmd.none)
+
+  
+nextFrame : Posix -> Model -> ( Model, Cmd Msg )
+nextFrame time model =
+  let time_ = Time.posixToMillis time in
+  if time_ - model.lastUpdate >= 1000 then
+    updateSnake model
+    |> Setters.setTime time_
+    |> Setters.setLastUpdate time_
+    |> Update.none
+  else
+    time_
+    |> Setters.setTimeIn model
+    |> Update.none
+
+{-| Main update function, mainly used as a router for subfunctions -}
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    ToggleGameLoop -> toggleGameLoop model
+    KeyDown key -> keyDown key model
+    NextFrame time -> nextFrame time model
+
+{-| Manage all your view functions here. -}
+cell : Bool -> Html msg
+cell active =
+  let class = if active then "cell active" else "cell" in
+  Html.div [ Attributes.class class ] []
+  
+
+movingSquare : Model -> Html msg
+movingSquare model =
+  Html.div [ Attributes.class "grid" ]
+    (List.concat 
+      (List.indexedMap (\y elem ->
+        List.indexedMap (\x _ ->
+          (cell (List.member (x, y) model.snake.cases))
+        )
+        elem
+       ) 
+        (List.repeat 40 (List.repeat 40 0))
+      )
+    )
+
+actualTime : Model -> Html msg
+actualTime { time } =
+  Html.div [ Attributes.class "actual-time" ]
+    [ Html.text "Actual time"
+    , time
+      |> String.fromInt
+      |> Html.text
+      |> List.singleton
+      |> Html.code []
+    ]
+
+explanations : Model -> Html Msg
+explanations ({ gameStarted } as model) =
+  let word = if gameStarted then "Stop" else "Start" in
+  Html.div [ Attributes.class "separator" ]
+    [ Html.h1 []
+      [ Html.text "Welcome to the snake project!" ]
+    , actualTime model
+    , Html.button
+      [ Events.onClick ToggleGameLoop, Attributes.class "btn" ]
+      [ Html.text (String.join " " [word, "game loop"]) ]
+    ]
+
+{-| Main view functions, composing all functions in one -}
+view : Model -> Html Msg
+view model =
+  Html.main_ []
+    [ Html.img [ Attributes.src "/logo.svg" ] []
+    , explanations model
+    , movingSquare model
+    ]
+
+{-| Parts for the runtime. Get key presses and subscribe to
+ -|   requestAnimationFrame for the game loop. You don't have to bother with
+ -|   this. -}
+decodeArrow : String -> Decode.Decoder Key
+decodeArrow value =
+  case value of
+    "ArrowUp" -> Decode.succeed ArrowUp
+    "ArrowLeft" -> Decode.succeed ArrowLeft
+    "ArrowRight" -> Decode.succeed ArrowRight
+    "ArrowDown" -> Decode.succeed ArrowDown
+    " " -> Decode.succeed Space
+    _ -> Decode.fail "Not an arrow"
+
+decodeKey : Decode.Decoder Msg
+decodeKey =
+  Decode.field "key" Decode.string
+  |> Decode.andThen decodeArrow
+  |> Decode.map KeyDown
+
+subscriptions : Model -> Sub Msg
+subscriptions { gameStarted } =
+  let aF = Browser.Events.onAnimationFrame NextFrame
+      base = Browser.Events.onKeyDown decodeKey :: [] in
+    Sub.batch (if gameStarted then aF :: base else base)
+
+{-| Entrypoint of your program -}
+main : Program Flags Model Msg
+main =
+  Browser.element
+    { view = view
+    , init = init
+    , update = update
+    , subscriptions = subscriptions
+    }
